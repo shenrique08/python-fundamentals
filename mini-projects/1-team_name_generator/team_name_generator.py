2b0545b7b7c48deaf557e7ec40b63c796c652699
shenrique08 <sergimmatos@hotmail.com>
2024-06-05 14:21:14 -0300

    first mini-project

--- mini-projects/1-team_name_generator/team_name_generator.py
+++ mini-projects/1-team_name_generator/team_name_generator.py
@@ -8,7 +8,9 @@ def generate_team_names(base_name):
     return team_name, team_name2
 
 
-user_input = str(input("Enter a base name for your footbal team: "))
+user_input = str(input("Enter a base name for your football team: "))
+size_of_string = len(user_input)
 
 team1, team2 = generate_team_names(user_input)
+print(f"Length of the input: {size_of_string}")
 print(f"Suggestions:\nTeam1: {team1}\nTeam2: {team2}")
